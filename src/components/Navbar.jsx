d17b32036cd81ad4fd677b654f58f34aad24513c
gitAdalberto <adalbertogit@gmail.com>
2025-05-15 21:10:51 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
+'use client';
 import Link from "next/link";
+import { useState } from "react";
 import styles from "@/components/Navbar.module.css";
 
 export default function Navbar() {
-    return (
-        <nav className={styles.nav}>
-            <h2 className={styles.title}><Link href="/">Taller</Link></h2>
-            <ul className={styles.ul}>
-                <li><Link href="/">Inicio</Link></li>
-                <li><Link href="/client">Clientes</Link></li>
-                <li><Link href="/mechanic">Mecánicos</Link></li>
-                <li><Link href="/order">Ordenes</Link></li>
-            </ul>
-        </nav>
-    )
-};
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.nav}>
+      <h2 className={styles.title}><Link href="/">Taller</Link></h2>
+
+      <button className={styles.menuButton} onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <ul className={`${styles.ul} ${menuOpen ? styles.show : ""}`}>
+        <li><Link href="/">Inicio</Link></li>
+        <li><Link href="/client">Clientes</Link></li>
+        <li><Link href="/mechanic">Mecánicos</Link></li>
+        <li><Link href="/order">Órdenes</Link></li>
+      </ul>
+    </nav>
+  );
+}
